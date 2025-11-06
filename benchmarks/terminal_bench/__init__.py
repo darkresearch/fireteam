3b3fc2ba7be5ce7ec370062f17f86f4295998ae3
edgarpavlovsky <edgarpavlovsky@users.noreply.github.com>
2025-11-05 23:21:38 -0700

    cleaning benchmark tooling

--- benchmarks/terminal_bench/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Terminal Bench integration for Fireteam.
-
-This package provides the integration layer between Fireteam and Terminal Bench,
-allowing Fireteam to be benchmarked against industry-standard coding tasks.
-"""
-
-__version__ = "0.1.0"
